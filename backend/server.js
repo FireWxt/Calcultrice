b7a29cf07d94c7fac8da0ffff470b377d1226b84
FireWat <semiaosimon@gmail.com>
2026-05-12 15:57:17 +0200

    fixing back

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,6 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
